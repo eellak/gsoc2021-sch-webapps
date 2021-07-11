4a1b4e76fcb454409e63ef6871751babdcf3ef2c
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-07-11 13:37:43 +0300

    Fix identation errors and newlines (#6)

--- frontendjs.js
+++ frontendjs.js
@@ -1,73 +1,73 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById('mySidebar').style.width = '250px'
-    document.getElementById('container').style.marginLeft = '250px'
+  document.getElementById('mySidebar').style.width = '250px'
+  document.getElementById('container').style.marginLeft = '250px'
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById('mySidebar').style.width = '0'
-    document.getElementById('container').style.marginLeft = '0'
+  document.getElementById('mySidebar').style.width = '0'
+  document.getElementById('container').style.marginLeft = '0'
 }
 
 // load dynamically into container every app needed
 function buildApp(appDescription, appURL, appImg) {
-    var appContainer = document.getElementById('app-container')
-    console.log(appContainer.childNodes)
+  var appContainer = document.getElementById('app-container')
+  console.log(appContainer.childNodes)
 
-    var app = document.createElement('div')
-    app.classList.add('app')
+  var app = document.createElement('div')
+  app.classList.add('app')
 
-    var a = document.createElement('a')
-    a.href = appURL
-    a.target = '_blank'
+  var a = document.createElement('a')
+  a.href = appURL
+  a.target = '_blank'
 
-    var imageContainer = document.createElement('div')
-    imageContainer.classList.add('image-container')
+  var imageContainer = document.createElement('div')
+  imageContainer.classList.add('image-container')
 
-    var overlay = document.createElement('div')
-    overlay.classList.add('overlay')
+  var overlay = document.createElement('div')
+  overlay.classList.add('overlay')
 
-    var startIcon = document.createElement('div')
-    startIcon.classList.add('start-icon')
+  var startIcon = document.createElement('div')
+  startIcon.classList.add('start-icon')
 
-    var clickStart = document.createElement('div')
-    clickStart.classList.add('click-start')
-    clickStart.textContent = 'Click to start'
+  var clickStart = document.createElement('div')
+  clickStart.classList.add('click-start')
+  clickStart.textContent = 'Click to start'
 
-    overlay.appendChild(startIcon)
-    overlay.appendChild(clickStart)
+  overlay.appendChild(startIcon)
+  overlay.appendChild(clickStart)
 
-    var appImage = document.createElement('div')
-    appImage.classList.add('app-image')
-    appImage.style.backgroundImage = 'url(' + appImg + ')'
+  var appImage = document.createElement('div')
+  appImage.classList.add('app-image')
+  appImage.style.backgroundImage = 'url(' + appImg + ')'
 
-    imageContainer.appendChild(overlay)
-    imageContainer.appendChild(appImage)
+  imageContainer.appendChild(overlay)
+  imageContainer.appendChild(appImage)
 
-    var appTitle = document.createElement('div')
-    appTitle.classList.add('app-title')
-    appTitle.textContent = appDescription
+  var appTitle = document.createElement('div')
+  appTitle.classList.add('app-title')
+  appTitle.textContent = appDescription
 
-    a.appendChild(imageContainer)
-    a.appendChild(appTitle)
+  a.appendChild(imageContainer)
+  a.appendChild(appTitle)
 
-    app.appendChild(a)
-    appContainer.appendChild(app)
+  app.appendChild(a)
+  appContainer.appendChild(app)
 }
 
 function loadApps() {
-    for (i = 0; i < packages.length; i++) {
-        description = packages[i].description
-        tmp0 = packages[i].homepage
-        tmp1 = tmp0.split('/')
-        tmp2 = tmp1[4].split('#')[0]
-        url = 'https://photodentro.github.io/' + tmp2 + '/'
-        console.log(url)
-        img = url + 'package/256x144.png'
-        console.log(img)
+  for (i = 0; i < packages.length; i++) {
+    description = packages[i].description
+    tmp0 = packages[i].homepage
+    tmp1 = tmp0.split('/')
+    tmp2 = tmp1[4].split('#')[0]
+    url = 'https://photodentro.github.io/' + tmp2 + '/'
+    console.log(url)
+    img = url + 'package/256x144.png'
+    console.log(img)
 
-        buildApp(description, url, img)
-    }
-    document.getElementById('demo-app').style.display = "none"
+    buildApp(description, url, img)
+  }
+  document.getElementById('demo-app').style.display = "none"
 }
